ea3c98948633eb21ae0e899cf9b3acf2dbaa3a70
sheepman <sheepman@mail.ustc.edu.cn>
2025-04-11 21:02:28 +0800

    添加了GUI界面实现代码和软件测试代码（main.py）

---
+++ main.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  7 17:30:34 2025
+
+@author: 28211
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  7 17:30:24 2025
+
+@author: 28211
+
+"""
+import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox
+from GUI_achieve import StockGUI
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = StockGUI(root)
+    root.mainloop()
